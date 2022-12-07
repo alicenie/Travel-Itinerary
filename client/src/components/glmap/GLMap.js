1c26c62d6e15e050d814b623508a7ff292303ff9
YvonneFang <112362682+y1vonnef@users.noreply.github.com>
2022-12-06 22:42:38 -0500

    enable dynamic loading of 3D map pins

--- client/src/components/glmap/GLMap.js
+++ client/src/components/glmap/GLMap.js
@@ -10,8 +10,8 @@ var keyPressed = null;
 //TODO replace with dynamic data
 const route = [
   { lat: 40.43901, lng: -79.94795 },
-  { lat: 40.43901, lng: -79.94795 },
-  { lat: 40.43901, lng: -79.94795 },
+  { lat: 40.4432, lng: -79.94284 },
+  { lat: 40.48426, lng: -79.9222 },
 ];
 // const GLMap = () => {
 const apiOptions = {
@@ -37,67 +37,30 @@ const mapSpeedLng = (-79.9222 - firstlng) / 4500;
 // console.log("speed is " + speedlat + ", " + speedlng);
 
 function initWebGLOverlayView(map) {
-  let scene,
-    renderer,
-    camera,
-    loader,
-    scene2,
-    camera2,
-    scene3,
-    camera3,
-    scene4,
-    camera4,
-    carModel;
+  let renderer, loader, scene4, camera4, carModel;
+  let scenes = [];
+  let cameras = [];
   const WebGLOverlayView = new window.google.maps.WebGLOverlayView();
 
   WebGLOverlayView.onAdd = () => {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera();
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-    scene.add(ambientLight);
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(0.5, -1, 0.5);
-    scene.add(directionalLight);
-    //loading a 3D model
     loader = new GLTFLoader();
-    // const source = "pin.gltf";
-    loader.load(pin, (gltf) => {
-      gltf.scene.scale.set(20, 20, 20);
-      gltf.scene.rotation.x = (180 * Math.PI) / 180;
-      scene.add(gltf.scene);
-    });
 
-    //2nd scene, 2nd camera, 2nd gltf model
-    //TODO make this into function so can be scaled
-    scene2 = new THREE.Scene();
-    camera2 = new THREE.PerspectiveCamera();
-    const ambientLight2 = new THREE.AmbientLight(0xffffff, 1);
-    const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight2.position.set(0.5, -1, 0.5);
-    scene2.add(ambientLight2);
-
-    scene2.add(directionalLight2);
-    //loading a 3D model
-    loader.load(pin, (gltf2) => {
-      gltf2.scene.scale.set(20, 20, 20);
-      gltf2.scene.rotation.x = (180 * Math.PI) / 180;
-      scene2.add(gltf2.scene);
-    });
-
-    scene3 = new THREE.Scene();
-    camera3 = new THREE.PerspectiveCamera();
-    const ambientLight3 = new THREE.AmbientLight(0xffffff, 1);
-    const directionalLight3 = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight3.position.set(0.5, -1, 0.5);
-    scene3.add(ambientLight3);
-    scene3.add(directionalLight3);
-    //loading a 3D model
-    loader.load(pin, (gltf3) => {
-      gltf3.scene.scale.set(20, 20, 20);
-      gltf3.scene.rotation.x = (180 * Math.PI) / 180;
-      scene3.add(gltf3.scene);
-    });
-
+    for (let i = 0; i < route.length; i++) {
+      scenes[i] = new THREE.Scene();
+      cameras[i] = new THREE.PerspectiveCamera();
+      const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+      scenes[i].add(ambientLight);
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+      directionalLight.position.set(0.5, -1, 0.5);
+      scenes[i].add(directionalLight);
+      //loading a 3D model
+      // const source = "pin.gltf";
+      loader.load(pin, (gltf) => {
+        gltf.scene.scale.set(20, 20, 20);
+        gltf.scene.rotation.x = (180 * Math.PI) / 180;
+        scenes[i].add(gltf.scene);
+      });
+    }
     scene4 = new THREE.Scene();
     camera4 = new THREE.PerspectiveCamera();
     const ambientLight4 = new THREE.AmbientLight(0xffffff, 2);
@@ -110,7 +73,6 @@ function initWebGLOverlayView(map) {
       gltf4.scene.rotation.x = (90 * Math.PI) / 180;
       gltf4.scene.rotation.y = (90 * Math.PI) / 180;
       carModel = gltf4.scene;
-      console.log(carModel);
       scene4.add(gltf4.scene);
     });
 
@@ -198,46 +160,48 @@ function initWebGLOverlayView(map) {
   //called every time we render
   WebGLOverlayView.onDraw = ({ gl, transformer }) => {
     WebGLOverlayView.requestRedraw();
-    renderer.render(scene, camera);
-    //render 2nd scene
-    renderer.render(scene2, camera2);
-
-    // update the picking ray with the camera and pointer position
-    // console.log("camera is " + camera3);
-    // calculate objects intersecting the picking ray
-
-    renderer.render(scene3, camera3);
+    for (let i = 0; i < route.length; i++) {
+      renderer.render(scenes[i], cameras[i]);
+    }
+    // renderer.render(scene, camera);
+    // renderer.render(scene2, camera2);
+    // renderer.render(scene3, camera3);
     renderer.render(scene4, camera4);
 
     renderer.resetState();
-    //positioning of 3D model & camera
-    const latLngAltitudeLiteral = {
-      lat: 40.43901,
-      lng: -79.94795,
-      altitude: 60,
-    };
-    const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
-    camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
+    for (let i = 0; i < route.length; i++) {
+      let latLngAltitudeLiteral = {
+        lat: route[i].lat,
+        lng: route[i].lng,
+        altitude: 60,
+      };
+      let matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
+      cameras[i].projectionMatrix = new THREE.Matrix4().fromArray(matrix);
+    }
+    // const latLngAltitudeLiteral = {
+    //   lat: 40.43901,
+    //   lng: -79.94795,
+    //   altitude: 60,
+    // };
+    // const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
+    // camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
 
-    //positioning of 2nd model
-    const latLngAltitudeLiteral2 = {
-      lat: 40.4432,
-      lng: -79.94284,
-      altitude: 60,
-    };
-    const matrix2 = transformer.fromLatLngAltitude(latLngAltitudeLiteral2);
-    camera2.projectionMatrix = new THREE.Matrix4().fromArray(matrix2);
+    // const latLngAltitudeLiteral2 = {
+    //   lat: 40.4432,
+    //   lng: -79.94284,
+    //   altitude: 60,
+    // };
+    // const matrix2 = transformer.fromLatLngAltitude(latLngAltitudeLiteral2);
+    // camera2.projectionMatrix = new THREE.Matrix4().fromArray(matrix2);
 
-    //positioning of 3rd model
-    const latLngAltitudeLiteral3 = {
-      lat: 40.48426,
-      lng: -79.9222,
-      altitude: 60,
-    };
-    const matrix3 = transformer.fromLatLngAltitude(latLngAltitudeLiteral3);
-    camera3.projectionMatrix = new THREE.Matrix4().fromArray(matrix3);
+    // const latLngAltitudeLiteral3 = {
+    //   lat: 40.48426,
+    //   lng: -79.9222,
+    //   altitude: 60,
+    // };
+    // const matrix3 = transformer.fromLatLngAltitude(latLngAltitudeLiteral3);
+    // camera3.projectionMatrix = new THREE.Matrix4().fromArray(matrix3);
 
-    //positioning of 4th model
     const latLngAltitudeLiteral4 = {
       lat: firstlat,
       lng: firstlng,
@@ -255,11 +219,6 @@ function initWebGLOverlayView(map) {
   // WebGLOverlayView code goes here
 }
 
-// const mapContainerStyle = {
-//   width: "50vw",
-//   height: "50vh",
-// };
-
 //render code
 async function initMap() {
   const mapDiv = document.getElementById("glmap");
@@ -273,8 +232,5 @@ const initGLMap = async () => {
   const map = await initMap();
   initWebGLOverlayView(map);
 };
-
-// return (<div id="GLMAP"></div>)
-// };
 
 export default initGLMap;
