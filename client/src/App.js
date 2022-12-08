15ebf48cd13bbfbead0901bb2990477e1e839042
alicenie <fnie9421@gmail.com>
2022-12-07 22:02:21 -0500

    add header and adjust layout

--- client/src/App.js
+++ client/src/App.js
@@ -66,24 +66,26 @@ function App() {
   if (!isLoaded) return "Loading Google Maps...";
   return (
     <div className="App">
+      <div className="header">Travel Itinerary U Illustrate</div>
       <GridLayout
         className="layout"
         layout={layout}
+        margin={[15, 10]}
         cols={12}
         rowHeight={30}
-        width={1200}
+        width={window.innerWidth}
       >
         <div key="map">
           <Map />
         </div>
         <div key="glmap">
-          <button
+          {/* <button
             onClick={() => {
               initGLMap();
             }}
           >
             Show Walkthrough
-          </button>
+          </button> */}
           <p
             style={{
               zIndex: "10",
